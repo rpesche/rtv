0871b6736f83edaab77151b79ed2d6d8ae8e822f
Michael Lazar <mlazar@intellisis.com>
2015-10-30 18:03:57 -0700

    Refactoring controller, still in progress.

---
+++ tests/test_controller.py
@@ -0,0 +1,50 @@
+from rtv.page import Controller
+
+try:
+    from unittest import mock
+except ImportError:
+    import mock
+
+def test_controller():
+
+    class ControllerA(Controller):
+        character_map = {}
+
+    class ControllerB(ControllerA):
+        character_map = {}
+
+    class ControllerC(ControllerA):
+        character_map = {}
+
+    @ControllerA.register('1')
+    def call_page(page):
+        return 'a1'
+
+    @ControllerA.register('2')
+    def call_page(page):
+        return 'a2'
+
+    @ControllerB.register('1')
+    def call_page(page):
+        return 'b1'
+
+    @ControllerC.register('2')
+    def call_page(page):
+        return 'c2'
+
+    page = mock.MagicMock()
+    controller_a = ControllerA(page)
+    controller_b = ControllerB(page)
+    controller_c = ControllerC(page)
+
+    assert controller_a.trigger('1') == 'a1'
+    assert controller_a.trigger('2') == 'a2'
+    assert controller_a.trigger('3') is None
+
+    assert controller_b.trigger('1') == 'b1'
+    assert controller_b.trigger('2') == 'a2'
+    assert controller_b.trigger('3') is None
+
+    assert controller_c.trigger('1') == 'a1'
+    assert controller_c.trigger('2') == 'c2'
+    assert controller_c.trigger('3') is None
